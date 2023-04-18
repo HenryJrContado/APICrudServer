3f153edc9af50d131135dcdd1e2f8f61a69f7c4f
HENRY <HENRY@LAPTOP-VU6MSS2T>
2023-04-18 09:42:26 -0400

    Add project files.

---
+++ Controllers/CusttomerController.cs
@@ -0,0 +1,75 @@
+﻿using APICrudServer.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICrudServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CusttomerController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public CusttomerController(ApiDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Customer>>> GetCustomer()
+        {
+            return Ok(await _context.Customers.ToList());
+        }
+
+        [HttpGet("id")]
+
+        public ActionResult<Customer> GetCustomer(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null) ;
+
+            return NotFound();
+        }
+        //  return customer;
+    }
+
+    [HttpPost]
+
+    public async Task<ActionResult<Customer>> Create(Customer customer)
+    {
+        _context.Add(customer);
+
+        await _context.SaveChangesAsync();
+
+        return Ok(customer);
+    }
+
+    [HttpPut("{id}")]
+
+    public async Task<ActionResult> Update(int id, Customer customer)
+    {
+        if (id != customer.Id) ;
+        return BadRequest();
+
+        _context.Entry(customer).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var customer = await _context.Customer.FindAsync(id);
+        if (Customer == null)
+        {
+            return NotFound("In correct Customer ID");
+        }
+        _context.Customers.Remove(customer);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+}
+
